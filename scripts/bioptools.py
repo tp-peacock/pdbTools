fecb496da2f22bd835072e2a3063cb9593969316
Tom Peacock <thomas.s.peacock@gmail.com>
2018-02-01 15:50:56 +0000

    initial commit

---
+++ scripts/bioptools.py
@@ -0,0 +1,118 @@
+import sys
+import subprocess
+	
+def pdbsymm(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbsymm',toolarg])
+def pdbselect(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbselect',toolarg])
+def pdbhadd(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbhadd',toolarg])
+def pdbflip(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbflip',toolarg])
+def pdbcter(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbcter',toolarg])
+def chaincontacts(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/chaincontacts',toolarg])
+def pdbgetresidues(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbgetresidues',toolarg])
+def pdbfindresrange(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbfindresrange',toolarg])
+def pdbtranslate(toolarg):
+	#toolarg should be in format ["x", "y", "z", "infile", "outfile"]
+	#if outfile blank, will return to STDOUT
+	if len(toolarg) == 4:
+		return subprocess.call(['/home/tom/phd/bioptools/src/pdbtranslate',"-x ",str(toolarg[0]),"-y ",str(toolarg[1]),"-z ",str(toolarg[2]),toolarg[3]])
+	elif len(toolarg) == 5:
+		from IPython import embed
+		#embed()
+		return subprocess.call(['/home/tom/phd/bioptools/src/pdbtranslate',"-x ",str(toolarg[0]),"-y ",str(toolarg[1]),"-z ",str(toolarg[2]),toolarg[3],toolarg[4]])
+	else:
+		print "Error in pdbtranslate: wrong number of arguments"
+		sys.exit()
+def setpdbnumbering(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/setpdbnumbering',toolarg])
+def pdbtorsions(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbtorsions',toolarg])
+def pdbrenum(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbrenum',toolarg])
+def pdbrotate(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbrotate',toolarg])
+def pdbgetzone(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbgetzone',toolarg])
+def pdbfit(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbfit',toolarg])
+def pdbhstrip(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbhstrip',toolarg])
+def pdbaddhet(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbaddhet',toolarg])
+def pdbsolv(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbsolv',toolarg])
+def pdbpatchbval(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbpatchbval',toolarg])
+def pdbpatchnumbering(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbpatchnumbering',toolarg])
+def pdbsphere(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbsphere',toolarg])
+def pdb2xyz(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdb2xyz',toolarg])
+def pdbsecstr(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbsecstr',toolarg])
+def scorecons(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/scorecons',toolarg])
+def pdbhbond(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbhbond',toolarg])
+def pdb2pir(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdb2pir',toolarg])
+def pdbatomcount(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbatomcount',toolarg])
+def pdbatoms(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbatoms',toolarg])
+def pdbmakepatch(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbmakepatch',toolarg])
+def pdborder(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdborder',toolarg])
+def pdb2ms(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdb2ms',toolarg])
+def pdbcalcrms(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbcalcrms',toolarg])
+def pdbconect(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbconect',toolarg])
+def pdbheader(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbheader',toolarg])
+def pdborigin(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdborigin',toolarg])
+def pdbdummystrip(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbdummystrip',toolarg])
+def pdbsumbval(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbsumbval',toolarg])
+def pdbcount(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbcount',toolarg])
+def sixft(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/sixft',toolarg])
+def pdb2pdbml(toolarg):
+	return subprocess.call(['/home/tom/phd/bioptools/src/pdb2pdbml',toolarg])
+def pdbatomsel(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbatomsel',toolarg])
+def pdbavbr(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbavbr',toolarg])
+def pdbcheckforres(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbcheckforres',toolarg])
+def pdbml2pdb(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbml2pdb',toolarg])
+def pdbchain(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbchain',toolarg])
+def pdblistss(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdblistss',toolarg])
+def naccess2bval(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/naccess2bval',toolarg])
+def distmat(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/distmat',toolarg])
+def pdbhetstrip(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbhetstrip',toolarg])
+def pdbsplitchains(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbsplitchains',toolarg])
+def pdbcentralres(toolarg):
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbcentralres',toolarg])
+def pdbgetchain(toolarg):
+	#toolarg should be in form ["chain","file"] e.g. toolarg = ["A", "example.pdb"]
+	return subprocess.check_output(['/home/tom/phd/bioptools/src/pdbgetchain',toolarg[0],toolarg[1]])
---
+++ scripts/exploder.py
@@ -0,0 +1,166 @@
+#Testing with BiopTools
+import sys
+import os
+import shutil
+import operator
+import subprocess
+import bioptools
+
+from IPython import embed
+
+from numpy import mean
+
+# Function uses to quickly write the script bioptools.py, which allows bioptool C scripts to be
+# easily accessed through a python module
+def printFunctionsForTools():
+	biopToolsDir = "/home/tom/phd/bioptools/src"
+	for file in os.listdir(biopToolsDir):
+    	 if file.endswith(".c"):
+        	functionname = os.path.splitext(file)[0]
+         	print "def "+functionname+"(toolarg):"
+         	print "	return subprocess.check_output(['"+biopToolsDir+"/"+functionname+"',toolarg])"
+
+
+def separateStructure(pdb_dir,base_name):
+	directory = pdb_dir+"/tmp_dir"
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	else:
+		print "Error! "+directory+" exists and cannot be overwritten."
+		sys.exit()
+	
+	cwd = os.getcwd()
+	os.chdir(directory)
+	shutil.copy2("../"+base_name,directory)	
+	chains = bioptools.pdbsplitchains(base_name)
+	cleanUp(base_name)
+	
+	sepfiles = os.listdir(directory)
+	os.chdir(cwd)
+
+	return directory, sepfiles
+
+def cleanUp(rubbish):
+	if os.path.isfile(rubbish):
+		os.remove(rubbish)
+	elif os.path.isdir(rubbish):
+		shutil.rmtree(rubbish)
+
+def getCentralRes(pdbfile):
+	return bioptools.pdbcentralres(pdbfile).split("\n")[0].strip()
+
+def getChain(centroidID):
+	return centroidID[0]
+
+def getAtomID(centroidID):
+	return centroidID[1:]
+
+def getAtoms(pdbfile):
+	pdbatoms = bioptools.pdbatoms(pdbfile).split("\n")
+	atoms = []
+	for atom in pdbatoms:
+		split_atom = atom.split(" ")
+		split_atom = [elem for elem in split_atom if elem.strip()]
+		if len(split_atom) < 12:
+			continue
+		atoms.append(split_atom)
+	return atoms
+
+
+def getCentroid(pdbfile):
+	centroidID = getCentralRes(pdbfile)
+	
+	atoms = getAtoms(pdbfile)
+
+	chain = getChain(centroidID)
+	atomid = getAtomID(centroidID)
+	residueX = []
+	residueY = []
+	residueZ = []
+	for atom in atoms:
+		 if  atom[4] == chain and atom[5] == atomid:
+			residueX.append(float(atom[6]))
+	 		residueY.append(float(atom[7]))
+		 	residueZ.append(float(atom[8]))
+
+	centroid = (mean(residueX), mean(residueY), mean(residueZ))
+	print centroidID, centroid
+
+	return centroid
+
+def getDistance(point1, point2):
+	return map(operator.sub, point2, point1)
+
+def translateRes(dist, mult, file):
+	x = dist[0]*mult
+	y = dist[1]*mult
+	z = dist[2]*mult
+	outfile = os.path.splitext(file)[0]+"trans.pdb"
+	bioptools.pdbtranslate([x,y,z,file,outfile])
+	return outfile
+
+def appendFiles(masterfile,files):
+	m = open(masterfile, "w")
+
+	for file in files:
+		f = open(file,"r")
+   		for line in f:
+   			if "MASTER" in line:
+   				continue
+   			if "END" in line:
+   				continue
+			m.write(line)
+		f.close()
+	m.close()
+
+
+if __name__ == "__main__":
+
+	pdbfile = "/home/tom/phd/pdbTestFiles/1AO7.pdb"
+	pdb_dir = os.path.dirname(pdbfile)
+	base_name = os.path.basename(pdbfile)
+
+
+	#x = bioptools.pdbcount(pdbfile)
+	# chains = bioptools.pdbchain(pdbfile).split("\n")
+
+	# atom_count = 0
+	# for atom in chains:
+	# 	if "ATOM" not in atom:
+	# 		print atom
+	cleanUp("/home/tom/phd/pdbTestFiles/tmp_dir")
+
+	tmpdircontent = separateStructure(pdb_dir,base_name)
+	tmpdir = tmpdircontent[0]
+	
+	cres = getCentralRes(pdbfile)
+	centroid = getCentroid(pdbfile)
+
+	mult = 2
+	transfiles = []
+
+	for file in tmpdircontent[1:][0]:
+		pathfile = tmpdir+"/"+file
+		res = getCentralRes(pathfile)
+		if getChain(cres) == getChain(res):
+			centrefile = pathfile
+			transfiles.append(centrefile)
+			continue
+		else:
+			rescentre = getCentroid(pathfile)
+			dist = getDistance(centroid,rescentre)
+			tfile = translateRes(dist, mult, pathfile)
+			transfiles.append(tfile)
+
+	outfile = os.path.splitext(pdbfile)[0]+"exploded.pdb"
+	appendFiles(outfile, transfiles)
+	
+	# from IPython import embed
+	# embed()
+	
+	#print x
+	#print bioptools.pdbavbr(pdbfile)
+
+	#bioptools.pdb2pdbml(pdbfile)
+
+	cleanUp(tmpdir)
